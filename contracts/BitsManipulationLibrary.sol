aa2d2ff5e8f131bb281ad44eaba59a4399905d78
Danilo Tuler <danilo@cartesi.io>
2019-12-06 10:31:00 -0500

    Changed license headers to Apache 2

--- contracts/BitsManipulationLibrary.sol
+++ contracts/BitsManipulationLibrary.sol
@@ -10,7 +10,6 @@
 // specific language governing permissions and limitations under the License.
 
 
-
 pragma solidity ^0.5.0;
 
 /// @title Bits Manipulation Library
--- migrations/2_deploy_libraries.js
+++ migrations/2_deploy_libraries.js
@@ -1,12 +1,19 @@
+// Copyright 2019 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 
 const BitsManipulationLibrary = artifacts.require("BitsManipulationLibrary");
 const Merkle = artifacts.require("Merkle");
 
-module.exports = function(deployer, network, accounts) {
-
-    deployer.then(async () => {
-        await deployer.deploy(BitsManipulationLibrary);
-        await deployer.deploy(Merkle);
-    });
-
+module.exports = function(deployer) {
+    deployer.deploy(BitsManipulationLibrary);
+    deployer.deploy(Merkle);
 };
