80b33038da880bdc42d67b87e9096f562b977945
Danilo Tuler <danilo@cartesi.io>
2020-03-18 17:16:03 -0400

    Deploy configuration for Matic

--- truffle-config.js
+++ truffle-config.js
@@ -33,6 +33,10 @@ module.exports = {
     },
     ropsten: network('ropsten', 3),
     kovan: network('kovan', 42),
-    rinkeby: network('rinkeby', 4)
+    rinkeby: network('rinkeby', 4),
+    matic_testnet: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://testnetv3.matic.network'),
+      network_id: 15001
+    }
   }
 };
