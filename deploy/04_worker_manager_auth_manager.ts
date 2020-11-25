c6130d29e860f3f89f47596fefd8561d92b049ce
Danilo Tuler <danilo@cartesi.io>
2020-11-25 14:32:30 -0500

    Implementation of WorkerManager and WorkerAuthManager in a single contract

---
+++ deploy/04_worker_manager_auth_manager.ts
@@ -0,0 +1,26 @@
+// Copyright 2020 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the license at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy } = deployments;
+    const a = await getNamedAccounts();
+    const { deployer } = await getNamedAccounts();
+
+    await deploy("WorkerManagerAuthManagerImpl", { from: deployer, log: true });
+};
+
+export default func;
+export const tags = ["WorkerManagerAuthManager"];
