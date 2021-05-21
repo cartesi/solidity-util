549fca3d20401407b2476bb55145680577cb0928
Stephen Chen <stephen.chen@cartesi.io>
2021-05-21 21:00:03 +0800

    Add worker delegate

---
+++ worker/src/fold/worker_delegate.rs
@@ -0,0 +1,170 @@
+use crate::worker_lib::WorkerStatus;
+
+use super::contracts::worker_contract;
+
+use dispatcher::state_fold::{
+    delegate_access::{FoldAccess, SyncAccess},
+    error::*,
+    types::*,
+    utils as fold_utils,
+};
+use dispatcher::types::Block;
+
+use async_trait::async_trait;
+use ethers::types::Address;
+use snafu::ResultExt;
+
+/// Worker state, to be passed to and returned by fold.
+#[derive(Clone, Debug)]
+pub struct WorkerState {
+    pub status: WorkerStatus,
+}
+
+/// Worker StateFold Delegate, which implements `sync` and `fold`.
+pub struct WorkerFoldDelegate {
+    worker_manager_address: Address,
+    worker_address: Address,
+}
+
+impl WorkerFoldDelegate {
+    pub fn new(worker_manager_address: Address, worker_address: Address) -> Self {
+        WorkerFoldDelegate {
+            worker_manager_address,
+            worker_address,
+        }
+    }
+}
+
+#[async_trait]
+impl StateFoldDelegate for WorkerFoldDelegate {
+    type InitialState = ();
+    type Accumulator = WorkerState;
+    type State = BlockState<Self::Accumulator>;
+
+    async fn sync<A: SyncAccess + Send + Sync>(
+        &self,
+        _initial_state: &Self::InitialState,
+        block: &Block,
+        access: &A,
+    ) -> SyncResult<Self::Accumulator, A> {
+        let contract = access
+            .build_sync_contract(
+                self.worker_manager_address,
+                block.number,
+                worker_contract::WorkerManagerAuthManagerImpl::new,
+            )
+            .await;
+
+        // get last worker status transition event
+        let events = contract
+            .events()
+            .topic1(self.worker_address)
+            .query()
+            .await
+            .context(SyncContractError {
+                err: "Error querying for worker events",
+            })?;
+
+        println!("Sync events {:?}", events);
+
+        let last_event = events
+            .into_iter()
+            .filter(|e| match e {
+                // filter out the AuthManager events
+                worker_contract::WorkerManagerAuthManagerImplEvents::AuthorizationFilter(_)
+                | worker_contract::WorkerManagerAuthManagerImplEvents::DeauthorizationFilter(_) => {
+                    false
+                }
+                _ => true,
+            })
+            .last()
+            .map(|t| t.clone());
+
+        Ok(WorkerState {
+            status: compute_state(last_event, WorkerStatus::Available),
+        })
+    }
+
+    async fn fold<A: FoldAccess + Send + Sync>(
+        &self,
+        previous_state: &Self::Accumulator,
+        block: &Block,
+        access: &A,
+    ) -> FoldResult<Self::Accumulator, A> {
+        // Check if there was (possibly) some log emited on this block.
+        let bloom = block.logs_bloom;
+        if !(fold_utils::contains_address(&bloom, &self.worker_manager_address)
+            && fold_utils::contains_topic(&bloom, &self.worker_address))
+        {
+            return Ok(previous_state.clone());
+        }
+
+        let contract = access
+            .build_fold_contract(
+                self.worker_manager_address,
+                block.hash,
+                worker_contract::WorkerManagerAuthManagerImpl::new,
+            )
+            .await;
+
+        // get last status transition event
+        let events = contract
+            .events()
+            .topic1(self.worker_address)
+            .query()
+            .await
+            .context(FoldContractError {
+                err: "Error querying for worker events",
+            })?;
+
+        println!("Fold events {:?}", events);
+
+        let last_event = events
+            .into_iter()
+            .filter(|e| match e {
+                // filter out the AuthManager events
+                worker_contract::WorkerManagerAuthManagerImplEvents::AuthorizationFilter(_)
+                | worker_contract::WorkerManagerAuthManagerImplEvents::DeauthorizationFilter(_) => {
+                    false
+                }
+                _ => true,
+            })
+            .last()
+            .map(|t| t.clone());
+
+        Ok(WorkerState {
+            status: compute_state(last_event, previous_state.status.clone()),
+        })
+    }
+
+    fn convert(&self, state: &BlockState<Self::Accumulator>) -> Self::State {
+        state.clone()
+    }
+}
+
+/// Computes the state from the last event emission
+fn compute_state(
+    last_event: Option<worker_contract::WorkerManagerAuthManagerImplEvents>,
+    previous_status: WorkerStatus,
+) -> WorkerStatus {
+    match last_event {
+        // no relevant event
+        None => previous_status,
+        Some(event) => match event {
+            worker_contract::WorkerManagerAuthManagerImplEvents::JobAcceptedFilter(e) => {
+                WorkerStatus::Owned(e.user)
+            }
+            worker_contract::WorkerManagerAuthManagerImplEvents::JobOfferFilter(e) => {
+                WorkerStatus::Pending(e.user)
+            }
+            worker_contract::WorkerManagerAuthManagerImplEvents::JobRejectedFilter(_) => {
+                WorkerStatus::Available
+            }
+            worker_contract::WorkerManagerAuthManagerImplEvents::RetiredFilter(e) => {
+                WorkerStatus::Retired(e.user)
+            }
+            // shoudn't get here, authenticator events should be filter out already
+            _ => previous_status,
+        },
+    }
+}
---
+++ worker/src/worker_lib.rs
@@ -0,0 +1,80 @@
+use super::fold::contracts::worker_contract;
+
+use block_subscriber::NewBlockSubscriber;
+use transaction_manager::{
+    error::TxConversionError,
+    types::{ProviderFactory, ResubmitStrategy, Transaction, TransferValue},
+    TransactionManager,
+};
+
+use ethers::providers::Middleware;
+use ethers::types::Address;
+use std::sync::Arc;
+
+#[derive(Clone, Debug)]
+pub enum WorkerStatus {
+    Available,
+    Pending(Address),
+    Owned(Address),
+    Retired(Address),
+}
+
+impl WorkerStatus {
+    pub async fn react<M, PF, BS>(
+        &self,
+        worker_address: Address,
+        worker_manager_address: Address,
+        client: Arc<M>,
+        transaction_manager: &TransactionManager<PF, BS, String>,
+    ) -> std::result::Result<(), TxConversionError>
+    where
+        M: Middleware,
+        PF: ProviderFactory + Send + Sync + 'static,
+        BS: NewBlockSubscriber + Send + Sync + 'static,
+    {
+        let contract = worker_contract::WorkerManager::new(worker_manager_address, client);
+
+        match self {
+            WorkerStatus::Pending(user_address) => {
+                let tx = contract.accept_job().tx;
+
+                transaction_manager
+                    .send_transaction(
+                        format!("{}_accept_{}", worker_address, user_address),
+                        tx.from(worker_address),
+                        ResubmitStrategy {
+                            gas_multiplier: None,
+                            gas_price_multiplier: None,
+                            rate: 10,
+                        },
+                        1,
+                    )
+                    .await?;
+            }
+            WorkerStatus::Retired(user_address) => {
+                let tx = Transaction {
+                    from: worker_address,
+                    to: *user_address,
+                    value: TransferValue::All,
+                    call_data: None,
+                };
+
+                transaction_manager
+                    .send_transaction(
+                        format!("{}_retire_{}", worker_address, user_address),
+                        tx,
+                        ResubmitStrategy {
+                            gas_multiplier: None,
+                            gas_price_multiplier: None,
+                            rate: 10,
+                        },
+                        1,
+                    )
+                    .await?;
+            }
+            _ => {}
+        }
+
+        Ok(())
+    }
+}
