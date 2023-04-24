ff982453c9035dbf6beaaff2316961f70e10dc06
Danilo Tuler <danilo.tuler@cartesi.io>
2023-04-24 15:32:04 -0300

    chore: bump dependencies

--- test/TestLogarithm.ts
+++ test/TestLogarithm.ts
@@ -10,8 +10,6 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-global.crypto = require("crypto");
-
 import { BigNumber } from "ethers";
 import { expect } from "chai";
 import { deployments, ethers } from "hardhat";
