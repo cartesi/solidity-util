b8fc0a1419c5c70fffc5d8d82333e674769b4f33
Danilo Tuler <tuler@pobox.com>
2023-07-18 12:17:55 -0400

    feat: delete deprecated contracts

--- packages/contracts/contracts/BitsManipulation.sol
+++ packages/contracts/contracts/BitsManipulation.sol
@@ -16,13 +16,10 @@ pragma solidity ^0.8.0;
 /// @author Felipe Argento / Stephen Chen
 /// @notice Implements bit manipulation helper functions
 library BitsManipulation {
-
     /// @notice Sign extend a shorter signed value to the full int32
     /// @param number signed number to be extended
     /// @param wordSize number of bits of the signed number, ie, 8 for int8
-    function int32SignExtension(int32 number, uint32 wordSize)
-    public pure returns(int32)
-    {
+    function int32SignExtension(int32 number, uint32 wordSize) public pure returns (int32) {
         uint32 uNumber = uint32(number);
         bool isNegative = ((uint64(1) << (wordSize - 1)) & uNumber) > 0;
         uint32 mask = ((uint32(2) ** wordSize) - 1);
@@ -37,9 +34,7 @@ library BitsManipulation {
     /// @notice Sign extend a shorter signed value to the full uint64
     /// @param number signed number to be extended
     /// @param wordSize number of bits of the signed number, ie, 8 for int8
-    function uint64SignExtension(uint64 number, uint64 wordSize)
-    public pure returns(uint64)
-    {
+    function uint64SignExtension(uint64 number, uint64 wordSize) public pure returns (uint64) {
         uint64 uNumber = number;
         bool isNegative = ((uint64(1) << (wordSize - 1)) & uNumber) > 0;
         uint64 mask = ((uint64(2) ** wordSize) - 1);
@@ -53,14 +48,14 @@ library BitsManipulation {
 
     /// @notice Swap byte order of unsigned ints with 64 bytes
     /// @param num number to have bytes swapped
-    function uint64SwapEndian(uint64 num) public pure returns(uint64) {
-        uint64 output = ((num & 0x00000000000000ff) << 56)|
-            ((num & 0x000000000000ff00) << 40)|
-            ((num & 0x0000000000ff0000) << 24)|
+    function uint64SwapEndian(uint64 num) public pure returns (uint64) {
+        uint64 output = ((num & 0x00000000000000ff) << 56) |
+            ((num & 0x000000000000ff00) << 40) |
+            ((num & 0x0000000000ff0000) << 24) |
             ((num & 0x00000000ff000000) << 8) |
             ((num & 0x000000ff00000000) >> 8) |
-            ((num & 0x0000ff0000000000) >> 24)|
-            ((num & 0x00ff000000000000) >> 40)|
+            ((num & 0x0000ff0000000000) >> 24) |
+            ((num & 0x00ff000000000000) >> 40) |
             ((num & 0xff00000000000000) >> 56);
 
         return output;
@@ -68,9 +63,11 @@ library BitsManipulation {
 
     /// @notice Swap byte order of unsigned ints with 32 bytes
     /// @param num number to have bytes swapped
-    function uint32SwapEndian(uint32 num) public pure returns(uint32) {
-        uint32 output = ((num >> 24) & 0xff) | ((num << 8) & 0xff0000) | ((num >> 8) & 0xff00) | ((num << 24) & 0xff000000);
+    function uint32SwapEndian(uint32 num) public pure returns (uint32) {
+        uint32 output = ((num >> 24) & 0xff) |
+            ((num << 8) & 0xff0000) |
+            ((num >> 8) & 0xff00) |
+            ((num << 24) & 0xff000000);
         return output;
     }
 }
-
