f23a5f60121768aa3727cc937c3f823ce7c889d5
Danilo Tuler <tuler@pobox.com>
2023-07-18 09:31:01 -0400

    feat: turning on deterministic deployment for all contracts

--- packages/contracts/deploy/02_worker_manager.ts
+++ packages/contracts/deploy/02_worker_manager.ts
@@ -11,14 +11,23 @@
 // under the License.
 
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { DeployFunction, DeployOptions } from "hardhat-deploy/types";
 
-const func: DeployFunction = async (bre: HardhatRuntimeEnvironment) => {
-    const { deployments, getNamedAccounts } = bre;
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts, network } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    await deploy("WorkerManagerImpl", { from: deployer, log: true });
+    // IoTeX does not support the deterministic deployment through the contract used by hardhat-deploy
+    const deterministicDeployment = network.name !== "iotex_testnet";
+
+    const opts: DeployOptions = {
+        deterministicDeployment,
+        from: deployer,
+        log: true,
+    };
+
+    await deploy("WorkerManagerImpl", opts);
 };
 
 func.tags = ["WorkerManager"];
--- packages/contracts/deploy/06_logarithms.ts
+++ packages/contracts/deploy/06_logarithms.ts
@@ -11,17 +11,23 @@
 // under the License.
 
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { DeployFunction, DeployOptions } from "hardhat-deploy/types";
 
-const func: DeployFunction = async (bre: HardhatRuntimeEnvironment) => {
-    const { deployments, getNamedAccounts } = bre;
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts, network } = hre;
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    await deploy("UnrolledCordic", {
+    // IoTeX does not support the deterministic deployment through the contract used by hardhat-deploy
+    const deterministicDeployment = network.name !== "iotex_testnet";
+
+    const opts: DeployOptions = {
+        deterministicDeployment,
         from: deployer,
         log: true,
-    });
+    };
+
+    await deploy("UnrolledCordic", opts);
 };
 
 func.tags = ["Logarithms"];
