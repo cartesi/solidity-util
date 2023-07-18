1877e614cf6d6b03742c846b4acdc999591e514b
Danilo Tuler <tuler@pobox.com>
2023-07-18 12:20:33 -0400

    feat: remove copyright year

--- packages/contracts/test/TestCartesiMath.ts
+++ packages/contracts/test/TestCartesiMath.ts
@@ -1,5 +1,3 @@
-// Copyright 2021 Cartesi Pte. Ltd.
-
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
 // of the license at http://www.apache.org/licenses/LICENSE-2.0
